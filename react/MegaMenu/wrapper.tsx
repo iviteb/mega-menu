9f07fdba1dca98dc8204982b5d4be031893ec12a
RobertIviteb <robert@iviteb.com>
2022-08-01 13:37:38 +0300

    seller ID fix

--- react/MegaMenu/wrapper.tsx
+++ react/MegaMenu/wrapper.tsx
@@ -15,27 +15,9 @@ import { megaMenuState } from './State'
 const Wrapper: StorefrontFunctionComponent<MegaMenuProps> = (props) => {
   const { orientation } = props
 
-  // const { data: session } = useFullSession()
-
-  // console.log('🚀 ~ file: wrapper.tsx ~ line 19 ~ session', session)
-
-  // console.log(
-  //   '🚀 ~ file: wrapper.tsx ~ line 19 ~ session',
-  //   session?.namespaces?.public?.regionId?.value
-  // )
-
-  // const { orderForm } = useOrderForm()
-  // const formSellers: string[] = []
-
-  // orderForm?.items.forEach((item: any) => {
-  //   formSellers.push(item.seller)
-  // })
-
   const { data } = useQuery<MenusResponse>(GET_MENUS, {
     ssr: true,
   })
-
-  // console.log('🚀 ~ file: wrapper.tsx ~ line 38 ~ error', error)
 
   const { setDepartments, setConfig } = megaMenuState
 
