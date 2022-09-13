6a83f16c43657bceda0c0f11481e4e5db47e2bf3
RobertIviteb <robert@iviteb.com>
2022-09-13 13:06:55 +0300

    CR change

--- node/clients/region.ts
+++ node/clients/region.ts
@@ -1,22 +1,6 @@
-import type { InstanceOptions, IOContext } from '@vtex/api'
-import { ExternalClient } from '@vtex/api'
+import { JanusClient } from '@vtex/api'
 
-const ORDER_BASE_URL = (accountName: string) =>
-  `http://${accountName}.myvtex.com`
-
-export default class RegionClient extends ExternalClient {
-  constructor(context: IOContext, options?: InstanceOptions) {
-    super(ORDER_BASE_URL(context.account), context, {
-      ...options,
-      headers: {
-        ...options?.headers,
-        'Proxy-Authorization': context.authToken,
-        VtexIdclientAutCookie: context.authToken,
-        'X-Vtex-Use-Https': 'true',
-      },
-    })
-  }
-
+export default class RegionClient extends JanusClient {
   public async getRegionSellers(regionID: string): Promise<any> {
     return this.http.get(`/api/checkout/pub/regions/${regionID}`)
   }
